422611d52293fe6a8f5250019f9417935fffd7a9
zai-i <zainab@wonderbly.com>
2023-01-06 12:57:26 +0000

    Fix

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -8,7 +8,6 @@ export default () => (
       <div className="container">
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
         </div>
       </div>
     </section>
