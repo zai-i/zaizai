4bcf41986bcc583523eb295c6828658b1bcf8bd5
zai-i <zainab@wonderbly.com>
2023-01-06 15:53:35 +0000

    Remove 'content'

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -39,7 +39,7 @@ export const IndexPageTemplate = ({
                   </div>
                   <div className="column is-12">
                     <h3 className="has-text-weight-semibold">
-                      Latest content
+                      Latest
                     </h3>
                     <BlogRoll />
                     <div className="column is-12 has-text-centered">
