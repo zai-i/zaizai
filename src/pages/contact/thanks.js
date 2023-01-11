d0f9dc7421a2df026b76adfc416e45c1054b45c7
zai-i <zainab@wonderbly.com>
2023-01-11 13:47:43 +0000

    Style message received page

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -4,10 +4,14 @@ import Layout from "../../components/Layout";
 // eslint-disable-next-line
 export default () => (
   <Layout>
-    <section className="section">
+    <section className="section section--gradient">
       <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+                Thanks for your message!
+            </div>
+          </div>
         </div>
       </div>
     </section>
