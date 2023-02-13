80b875f648eb937976a61dff132a29d44ca7f71f
zai-i <zainab@wonderbly.com>
2023-02-13 10:18:59 +0000

    remove unused packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-dark-mode',
-    'gatsby-plugin-styled-components',
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-sass",
