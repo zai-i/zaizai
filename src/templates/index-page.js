75468daa4b4a1cd35caa42abe2a4378e7d117fca
zai-i <zainab@wonderbly.com>
2023-06-08 21:42:39 +0100

    Text

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -90,7 +90,7 @@ export const IndexPageTemplate = ({
           <div className="sketch">
           <SketchWrapper sketch={Sketch}/>
           </div>
-          <span>Dreaming up more plans!<br/>Draw something in the empty space</span>
+          <span>Dreaming up more plans!<br/>Draw something new in the empty space</span>
           </div>
           </div>
           <div className='column has-text-centered'>
