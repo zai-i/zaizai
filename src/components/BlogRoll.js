ed914405b935beabcd2a48f5c95dd2bb88f9dea5
zai-i <zainab@wonderbly.com>
2023-06-13 22:27:40 +0100

    Updates

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -8,7 +8,7 @@ class BlogRollTemplate extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <div className="columns is-multiline">
+      <div className="columns is-multiline blog-roll">
         {posts &&
           posts.map(({ node: post }) => (
             <div className="is-parent column is-6" key={post.id}>
