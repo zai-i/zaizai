8e95b1b3fa123898845e355636f8aa8c92fc325e
zai-i <zainab@wonderbly.com>
2023-07-18 11:30:38 +0100

    Icons

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -12,7 +12,7 @@ class BlogRollTemplate extends React.Component {
       <div className="columns is-multiline blog-roll">
         {posts &&
           posts.map(({ node: post }) => (
-            <FadeInSection isTiled>
+            <FadeInSection isTiled key={post.id}>
             <div className="is-parent column is-12" key={post.id}>
               <article
                 className="blog-list-item tile is-child box notification"
