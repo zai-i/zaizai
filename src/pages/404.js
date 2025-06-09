a3f1025825ac506b5533a2752fadd7daf94c803d
zai-i <zainab@wonderbly.com>
2025-06-09 09:54:51 +0100

    Revert 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,30 +1,13 @@
-import React from "react";
+import * as React from "react";
 import Layout from "../components/Layout";
 
-import FadeInSection from "../components/FadeInSection";
-
-// eslint-disable-next-line
-const NotFoundPage = () => {
-  return (
-    <Layout>
-      <FadeInSection>
-        <section className="section section--gradient">
-          <div className="container">
-            <div className="columns">
-              <div className="column is-10 is-offset-1">
-                <div className="section">
-                  <h1 className="title is-size-3 is-bold-light">
-                    {title}
-                  </h1>
-                  <PageContent className="content" content={content} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </FadeInSection>
-    </Layout>
-  );
-};
+const NotFoundPage = () => (
+  <Layout>
+    <div>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist… the sadness.</p>
+    </div>
+  </Layout>
+);
 
 export default NotFoundPage;
